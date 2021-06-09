8d75ef33fe886056beba78e0fe547ff3bc1905a2
oubush <oleg.u.bushuev@gmail.com>
2021-06-09 21:28:21 +0300

    solve tasks in linear_algebra

--- linear_algebra/myfunc.py
+++ linear_algebra/myfunc.py
@@ -1,3 +1,31 @@
 """Модуль с вспомогательными функциями для решения задач линейной
 алгебры.
 """
+import numpy as np
+
+def det(m):
+    """Считает определитель матрицы с целыми элементами"""
+    m = np.array(m)
+    return round(np.linalg.det(m))
+
+def det_float(m):
+    """Считает определитель матрицы с любыми элементами"""
+    m = np.array(m)
+    return np.linalg.det(m)
+
+def det_solve_2(m, b):
+    """Решение системы c 2 неизвестными по методу Крамера"""
+    m = np.array(m)
+    det_1 = det(np.column_stack([b, m[:, 1]]))
+    det_2 = det(np.column_stack([m[:, 0], b]))
+        
+    return (det_1 / det(m), det_2 / det(m))
+
+def det_solve_3(m, b):
+    """Решение системы c 3 неизвестными по методу Крамера"""
+    m = np.array(m)
+    det_1 = det(np.column_stack([b, m[:, 1], m[:, 2]]))
+    det_2 = det(np.column_stack([m[:, 0], b, m[:, 2]]))
+    det_3 = det(np.column_stack([m[:, 0], m[:, 1], b]))
+        
+    return (det_1 / det(m), det_2 / det(m), det_3 / det(m))
